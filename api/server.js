b5e21af0f965e13f3a441e45fa6039a159ffbccd
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-30 15:35:48 -0400

    posting a new user passing

--- api/server.js
+++ api/server.js
@@ -1,7 +1,31 @@
 // BUILD YOUR SERVER HERE
 const express = require("express");
 const User = require("./users/model");
+
 const server = express();
+server.use(express.json());
+
+//Posting a new user
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+  if (!user.name || !user.bio) {
+    res.status(400).json({
+      message: "Please provide name and bio for the user",
+    });
+  } else {
+    User.insert(user)
+      .then((createdUser) => {
+        res.status(201).json(createdUser);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "error creating user",
+          err: err.message,
+          stack: err.stack,
+        });
+      });
+  }
+});
 
 // Getting all users
 server.get("/api/users", (req, res) => {
